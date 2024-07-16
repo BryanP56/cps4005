15564761267d483154f1cce3104d4b1b5bd42421
BryanP56 <2313697@live.stmarys.ac.uk>
2024-07-16 19:33:16 +0100

    Database Connection

---
+++ LawfirmApp/src/main/java/LawfirmApp/database_connection.java
@@ -0,0 +1,20 @@
+package LawfirmApp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class database_connection {
+    private static final String URL = "jdbc:sqlite:resources/database.db";
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+}
